dd45335d419564d0d73fbffc8c07f338979a5872
Gunjan Singh <gunjansingh72002@gmail.com>
2026-04-30 18:29:29 +0530

    commit

--- Frontend/src/Components/Contact.jsx
+++ Frontend/src/Components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
     }
 
     try {
-      const res = await fetch(`${import.meta.env.VITE_API_URL}/send`, {
+      const res = await fetch("http://localhost:5000/api/send", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
