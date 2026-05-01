2244ef36fa4e18790ee2af61ce7862b949273f02
Gunjan Singh <gunjansingh72002@gmail.com>
2026-05-01 16:50:57 +0530

    commit

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://portfolioo-hop4.vercel.app/"
+  // "https://portfolioo-hop4.vercel.app/"
 ]
 
 app.use(cors({
